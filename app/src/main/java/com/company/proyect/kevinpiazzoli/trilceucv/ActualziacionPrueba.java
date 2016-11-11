304117c415b89532b5541b4d6bc11790e56edbab
EdwinCieza <ecieza.18@gmail.com>
2016-11-10 23:28:04 -0500

    aqui ess

---
+++ app/src/main/java/com/company/proyect/kevinpiazzoli/trilceucv/ActualziacionPrueba.java
@@ -0,0 +1,17 @@
+package com.company.proyect.kevinpiazzoli.trilceucv;
+
+/**
+ * Created by Edwin Cieza on 10/11/2016.
+ */
+
+public class ActualziacionPrueba {
+    //Se subio correctamente
+    //Ponga su contraseña
+    // dode?
+    //Ya esta que se sube
+    //Creo que salio mal jajaja era que se suba en mi repositorio o no?
+    // pues siii
+    //Mmm espere que veo algo
+    //No nada
+    //Creo que tengo que etntrar con mi repositorio
+}
